0da367055b2a2ebc2a5de6b3d0721a1b88a05bcd
Haimila Mikko H <mikkohai@cubbli20-gold.local>
2022-04-15 13:00:29 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -89,6 +89,7 @@ function createApp(database) {
   function trueOrfalse (date, holiday) {if (date && date.year === holiday.year && date.month === holiday.month && date.day === holiday.day) return true}
 
   
+
   function isHoliday(date) {
     const date2 = parseTemporalPlainDate(date.toISOString().split('T')[0]);
     const holidays = database.getHolidays();
