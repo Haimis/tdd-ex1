b6de0cb58ca09833d444b3ac876a04e9a0f8c642
Haimila Mikko H <mikkohai@cubbli20-gold.local>
2022-04-15 13:35:07 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -72,7 +72,7 @@ function createApp(database) {
   }
 
   function calculateReduction(date) {
-    console.log(date);
+    console.log(fromDate(date));
     let reduction = 0;
     if (date && isMonday2(fromDate(date)) && !isHoliday2(fromDate(date), database.getHolidays())) {
       reduction = 35;
