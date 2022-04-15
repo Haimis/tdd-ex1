65589ecaddfec9bbcf7d983cf3ab3162ef24d15d
Haimila Mikko H <mikkohai@cubbli20-gold.local>
2022-04-15 13:55:14 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -21,7 +21,7 @@ function createApp(database) {
     const baseCost = database.findBasePriceByType(type).cost;
     const date = parseDate(req.query.date);
     newdate = parseDate2(req.query.date);
-    const cost = calculateCost(age, type, date, baseCost);
+    const cost = calculateCost(age, type, newdate, baseCost);
     res.json({ cost });
   });
 
