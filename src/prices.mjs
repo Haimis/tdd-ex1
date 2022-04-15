8b7795114ea3902849797e8f80c84ad64cbd106b
Haimila Mikko H <mikkohai@cubbli20-gold.local>
2022-04-15 11:30:14 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -89,7 +89,7 @@ function createApp(database) {
       if (
         date &&
         date.getFullYear() === holiday2.year &&
-        date.getMonth() === holiday.getMonth() &&
+        date.getMonth() === holiday2.month - 1 &&
         date.getDate() === holiday.getDate()
       ) {
         return true;
