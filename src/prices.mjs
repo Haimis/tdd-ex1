5bbc4218df0c2de35a67fe2759721faae15ffc4a
Haimila Mikko H <mikkohai@cubbli20-gold.local>
2022-04-15 12:59:56 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -94,6 +94,7 @@ function createApp(database) {
     for (let row of holidays) {
       console.log(date2 in holidays)
       let holiday2 = parseTemporalPlainDate(row.holiday)
+      console.log(trueOrfalse(date2, holiday2))
       if (
         date2 &&
         date2.year === holiday2.year &&
